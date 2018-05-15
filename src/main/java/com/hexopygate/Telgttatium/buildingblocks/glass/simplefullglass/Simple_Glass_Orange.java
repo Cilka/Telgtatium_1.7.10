0a53c9debebff6a867f519f11f63b3a12f1c9ac8
Cilka <teraco15@gmail.com>
2018-05-15 01:52:52 -0500

    Added Simple Glass colored blocks and panes

---
+++ src/main/java/com/hexopygate/Telgttatium/buildingblocks/glass/simplefullglass/Simple_Glass_Orange.java
@@ -0,0 +1,8 @@
+package com.hexopygate.Telgttatium.buildingblocks.glass.simplefullglass;
+
+import com.hexopygate.Telgttatium.TelGlass;
+
+public class Simple_Glass_Orange extends TelGlass {
+
+}
+	
---
+++ src/main/java/com/hexopygate/Telgttatium/buildingblocks/glass/simplefullglass/panes/Simple_Glass_Cyan_Pane.java
@@ -0,0 +1,17 @@
+package com.hexopygate.Telgttatium.buildingblocks.glass.simplefullglass.panes;
+
+import com.hexopygate.Telgttatium.TelGlass;
+import com.hexopygate.Telgttatium.TelNormalGlassPane;
+
+public class Simple_Glass_Cyan_Pane extends TelNormalGlassPane {
+
+	public Simple_Glass_Cyan_Pane(String texture) {
+		super(texture);
+		// TODO Auto-generated constructor stub
+	}
+public Simple_Glass_Cyan_Pane(String faceTex, String trimTex) {
+		super(faceTex,trimTex);
+		// TODO Auto-generated constructor stub
+	}
+
+}
